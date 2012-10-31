ffc3e431adc6b41d1e556d2f31c263468443d481
Thane Vo <vothane@gmail.com>
2012-10-31 16:34:01 -0600

    fixed some tests, can't use alot of rspec's 'magic' on Java libraries

--- spec/aceshigh_spec.rb
+++ spec/aceshigh_spec.rb
@@ -25,9 +25,9 @@ describe 'acceshigh' do
     unstored  = ["Amsterdam has lots of bridges", "Venice has lots of canals"]
     text      = ["Amsterdam", "Venice"]
     
+    let(:doc)       { Lucene::Doc::Document.new }
     let(:directory) { Lucene::Store::RAMDirectory.new }
-    let(:indexer)   { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
-    let(:reader)    { Lucene::Index::IndexReader.open(directory) }
+    let(:indexer )    { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
     
     before(:all) do
       
@@ -38,24 +38,49 @@ describe 'acceshigh' do
         doc.add(Lucene::Doc::Field.new("contents", unstored[index], Lucene::Doc::Field::Store::NO, Lucene::Doc::Field::Index::ANALYZED))
         doc.add(Lucene::Doc::Field.new("city", text[index], Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::ANALYZED))
         indexer.add_document(doc);      
-      end     
-    end
-    
-    after(:all) do
-      reader.close
+      end 
+       
       indexer.close
-      directory.close
+       
     end
     
     it "should indexed the data" do
+      reader = Lucene::Index::IndexReader.open(directory)
       reader.maxDoc.should == ids.length
-      reader.numDocs.should == 1
+      reader.numDocs.should == ids.length
+      reader.close
+    end
+  end
+
+  context "when adding documents to an index" do
+      
+    ids       = ["1", "2"]
+    unindexed = ["Netherlands", "Italy"]
+    unstored  = ["Amsterdam has lots of bridges", "Venice has lots of canals"]
+    text      = ["Amsterdam", "Venice"]
+    
+    let(:doc)       { Lucene::Doc::Document.new }
+    let(:directory) { Lucene::Store::RAMDirectory.new }
+    let(:indexer )    { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
+    
+    before(:all) do
+      
+      ids.each_with_index do |item, index|
+        doc = Lucene::Doc::Document.new
+        doc.add(Lucene::Doc::Field.new("id", ids[index], Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::NOT_ANALYZED))
+        doc.add(Lucene::Doc::Field.new("country", unindexed[index], Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::NO))
+        doc.add(Lucene::Doc::Field.new("contents", unstored[index], Lucene::Doc::Field::Store::NO, Lucene::Doc::Field::Index::ANALYZED))
+        doc.add(Lucene::Doc::Field.new("city", text[index], Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::ANALYZED))
+        indexer.add_document(doc);      
+      end 
+       
     end
     
     it "should be able to delete indexed data" do
       indexer.delete_documents(Lucene::Index::Term.new("id", "1"))
       indexer.commit
       indexer.has_deletions.should be_true
+      indexer.close
     end   
-  end
+  end  
 end
