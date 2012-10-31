6d93f4cebdbbb52e3eb8894c725571d7a4e46bd1
Thane Vo <vothane@gmail.com>
2012-10-30 19:57:19 -0600

    some more testing

--- spec/aceshigh_spec.rb
+++ spec/aceshigh_spec.rb
@@ -25,9 +25,9 @@ describe 'acceshigh' do
     unstored  = ["Amsterdam has lots of bridges", "Venice has lots of canals"]
     text      = ["Amsterdam", "Venice"]
     
-    let(:doc)       { Lucene::Doc::Document.new }
     let(:directory) { Lucene::Store::RAMDirectory.new }
-    let(:indexer )    { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
+    let(:indexer)   { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
+    let(:reader)    { Lucene::Index::IndexReader.open(directory) }
     
     before(:all) do
       
@@ -38,18 +38,24 @@ describe 'acceshigh' do
         doc.add(Lucene::Doc::Field.new("contents", unstored[index], Lucene::Doc::Field::Store::NO, Lucene::Doc::Field::Index::ANALYZED))
         doc.add(Lucene::Doc::Field.new("city", text[index], Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::ANALYZED))
         indexer.add_document(doc);      
-      end 
-       
+      end     
+    end
+    
+    after(:all) do
+      reader.close
       indexer.close
-       
+      directory.close
     end
     
     it "should indexed the data" do
-      reader = Lucene::Index::IndexReader.open(directory)
       reader.maxDoc.should == ids.length
-      reader.numDocs.should == ids.length
-      reader.close
+      reader.numDocs.should == 1
     end
-
+    
+    it "should be able to delete indexed data" do
+      indexer.delete_documents(Lucene::Index::Term.new("id", "1"))
+      indexer.commit
+      indexer.has_deletions.should be_true
+    end   
   end
 end
