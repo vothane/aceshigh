82511a280cb6ddb1eaca7981ff2890ef769eec4d
Thane Vo <vothane@gmail.com>
2012-10-31 18:21:39 -0600

    more tests

--- spec/aceshigh_spec.rb
+++ spec/aceshigh_spec.rb
@@ -27,7 +27,7 @@ describe 'acceshigh' do
     
     let(:doc)       { Lucene::Doc::Document.new }
     let(:directory) { Lucene::Store::RAMDirectory.new }
-    let(:indexer )    { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
+    let(:indexer)   { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
     
     before(:all) do
       
@@ -52,7 +52,7 @@ describe 'acceshigh' do
     end
   end
 
-  context "when adding documents to an index" do
+  context "when deleting documents to an index" do
       
     ids       = ["1", "2"]
     unindexed = ["Netherlands", "Italy"]
@@ -61,7 +61,7 @@ describe 'acceshigh' do
     
     let(:doc)       { Lucene::Doc::Document.new }
     let(:directory) { Lucene::Store::RAMDirectory.new }
-    let(:indexer )    { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
+    let(:indexer)   { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
     
     before(:all) do
       
@@ -81,6 +81,49 @@ describe 'acceshigh' do
       indexer.commit
       indexer.has_deletions.should be_true
       indexer.close
+    end   
+  end  
+
+  context "when searching documents on an index" do
+      
+    ids       = ["1", "2"]
+    unindexed = ["Netherlands", "Italy"]
+    unstored  = ["Amsterdam has lots of bridges", "Venice has lots of canals"]
+    text      = ["Amsterdam", "Venice"]
+    
+    let(:doc)       { Lucene::Doc::Document.new }
+    let(:directory) { Lucene::Store::RAMDirectory.new }
+    let(:indexer)   { Lucene::Index::IndexWriter.new(directory, Lucene::Analysis::WhitespaceAnalyzer.new, Lucene::Index::IndexWriter::MaxFieldLength::UNLIMITED) }
+    let(:searcher)  { Lucene::Search::IndexSearcher.new(directory) }
+    
+    before(:all) do
+      
+      ids.each_with_index do |item, index|
+        doc = Lucene::Doc::Document.new
+        doc.add(Lucene::Doc::Field.new("id", ids[index], Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::NOT_ANALYZED))
+        doc.add(Lucene::Doc::Field.new("country", unindexed[index], Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::NO))
+        doc.add(Lucene::Doc::Field.new("contents", unstored[index], Lucene::Doc::Field::Store::NO, Lucene::Doc::Field::Index::ANALYZED))
+        doc.add(Lucene::Doc::Field.new("city", text[index], Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::ANALYZED))
+        indexer.add_document(doc);      
+      end 
+       
+    end
+    
+    it "should be able to update indexed data" do    
+      doc = Lucene::Doc::Document.new
+      doc.add(Lucene::Doc::Field.new("id", "1", Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::NOT_ANALYZED))
+      doc.add(Lucene::Doc::Field.new("country", "Netherlands", Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::NO))
+      doc.add(Lucene::Doc::Field.new("contents", "Den Haag has a lot of museums", Lucene::Doc::Field::Store::NO, Lucene::Doc::Field::Index::ANALYZED))
+      doc.add(Lucene::Doc::Field.new("city", "Den Haag", Lucene::Doc::Field::Store::YES, Lucene::Doc::Field::Index::ANALYZED))
+      
+      indexer.update_document(Lucene::Index::Term.new("id", "1"), doc);        
+      indexer.commit
+      #indexer.close
+          
+      term = Lucene::Index::Term.new("city", "Den Haag")
+      query = Lucene::Search::TermQuery.new(term)
+      searcher.search(query, 1).totalHits.should == 1
+      searcher.close
     end   
   end  
 end
