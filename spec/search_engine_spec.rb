f6a120903aab90be26b1e0b1f3a2008144754956
Thane Vo <vothane@gmail.com>
2013-05-04 17:36:00 -0600

    test for appropriate delegation for other index methods

--- spec/search_engine_spec.rb
+++ spec/search_engine_spec.rb
@@ -6,9 +6,24 @@ describe 'aceshigh search engine' do
 
     let(:search_engine) { Aces::High::SearchEngine.new('../../test_index') }
     
-    it "should delegate to indexer <<" do
+    it "should delegate to Lucene index <<" do
       Lucene::Index.any_instance.should_receive(:<<).with( {:id => '69', :content => 'kjbkjkj'} )
       search_engine.indexer.index( {:id => '69', :content => 'kjbkjkj'} )
+    end
+
+    it "should delegate to Lucene index clear" do
+      Lucene::Index.any_instance.should_receive(:clear)
+      search_engine.indexer.clear
+    end
+
+    it "should delegate to Lucene index commit" do
+      Lucene::Index.any_instance.should_receive(:commit)
+      search_engine.indexer.commit
+    end
+
+    it "should delegate to Lucene index findt" do
+      Lucene::Index.any_instance.should_receive(:find).with( :id => 69 )
+      search_engine.indexer.find( :id => 69 )
     end
   end
 end
