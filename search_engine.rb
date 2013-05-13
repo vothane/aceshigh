aac23285c6ac69f5a554900da7091781bfdcb97f
Thane Vo <vothane@gmail.com>
2013-05-12 18:24:59 -0600

    temporary hacks to get server to work

--- search_engine.rb
+++ search_engine.rb
@@ -4,16 +4,20 @@ require 'lib/aceshigh'
 module Aces
   module High
     class SearchEngine 
+      extend Forwardable
       include Indexable
       include IndexInformable
       include FieldInformable
 
       attr_accessor :indexer, :index_informer
+      
+      #def_delegators :@indexer, :index
 
       def initialize(params = {})
         @indexer        = index(params[:index_path])
+        binding.pry
         @index_informer = index_info(params[:index_path])
-        @field_informer = field_info(params[:fields])
+        @field_informer = field_info(params[:fields] || {})
       end
     end
   end
